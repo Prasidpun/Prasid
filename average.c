b7fbbb5cc2caa5342027d38fe848c7ee7859ff73
Prasidpun <60817038+Prasidpun@users.noreply.github.com>
2020-09-05 20:21:16 +0545

    Create average.c

---
+++ average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,average;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+average=(a+b)/2
+printf("the average of the two number is %d",&average);
+}
