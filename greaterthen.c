141c0c07f26819e41f36822e837294913c53c14f
Prasidpun <60817038+Prasidpun@users.noreply.github.com>
2020-09-05 20:18:07 +0545

    Create greaterthen.c

---
+++ greaterthen.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+if(a>b)
+{
+printf("The greatest number is %d",&a);
+}
+else
+{
+printf("The greatest number is %d",&b);
+}
+}
