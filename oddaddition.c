d08b6f4b6fe443b062d0664c09152c0347689a15
Prasidpun <60817038+Prasidpun@users.noreply.github.com>
2020-09-15 08:55:12 +0545

    Update oddaddition.c

--- oddaddition.c
+++ oddaddition.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int sum=0,n,i,a=1;
+    int sum=0,n,i;
     printf("enter a number");
     scanf("%d",&n);
     for(i=1;i<=n;i=i+2)
     {
-        printf("%d",&a);
-        sum=sum+a;
-        a=sum+2;
+        printf("%d",2*i-1);
+        sum=sum+2*i-1;
+      printf("\n");
     }
     
     return 0;
