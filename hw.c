f2edadd20e9c2285bf8810678affbff338ab2545
Prasidpun <60817038+Prasidpun@users.noreply.github.com>
2021-01-31 19:10:46 +0545

    Create hw.c

---
+++ hw.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+ int salary , saving , amount , a , t ;
+ printf("enter the monthly salary");
+ scanf("%d",salary);
+ printf("enter the amount he can save ");
+ scanf("%d",saving);
+ printf("enter the amount he wanna have at the time of retirement");
+ scanf("%d",amount);
+ for (t=0;a=amount;t=t+1)
+ {
+     a = a + saving;
+ }
+ printf("the time he will get retired %d ",t);
+ printf("the amount he will have at the time of retirement %d",amount);
+ 
+}
