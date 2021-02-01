4264ff965a921d0ecbd7d92825ed9eb932138e24
Prasidpun <60817038+Prasidpun@users.noreply.github.com>
2021-02-01 18:06:06 +0545

    Update hw.c

--- hw.c
+++ hw.c
@@ -4,11 +4,11 @@ int main()
 {
  int salary , saving , amount , a , t ;
  printf("enter the monthly salary");
- scanf("%d",salary);
+ scanf("%d",&salary);
  printf("enter the amount he can save ");
- scanf("%d",saving);
+ scanf("%d",&saving);
  printf("enter the amount he wanna have at the time of retirement");
- scanf("%d",amount);
+ scanf("%d",&amount);
  for (t=0;a<amount;t=t+1)
  {
      a = a + saving;
